f5674102161f4c45d899c1c2fd364e9a5315b493
kunani1 <131540652+kunani1@users.noreply.github.com>
2026-02-03 19:21:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from driver.veez import call_py, bot, user
 async def start_bot():
     await bot.start()
     print("[INFO]: BOT & UBOT CLIENT STARTED !!")
+
     await call_py.start()
     print("[INFO]: PY-TGCALLS CLIENT STARTED !!")
-    await user.join_chat("VeezSupportGroup")
-    await user.join_chat("levinachannel")
+
     await idle()
+
     print("[INFO]: STOPPING BOT & USERBOT")
     await bot.stop()
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(start_bot())
+
+if __name__ == "__main__":
+    asyncio.run(start_bot())
