e9eaf0794c429114043fb6b8948e0624d1c88d73
kunani1 <131540652+kunani1@users.noreply.github.com>
2026-02-03 19:44:24 +0530

    Refactor event loop and join user chats
    Updated event loop handling and added user chat joins.

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 import asyncio
-
-# Force event loop (required for pytgcalls on Heroku)
-try:
-    asyncio.get_running_loop()
-except RuntimeError:
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-
 from pytgcalls import idle
 from driver.veez import call_py, bot, user
 
@@ -14,15 +6,13 @@ from driver.veez import call_py, bot, user
 async def start_bot():
     await bot.start()
     print("[INFO]: BOT & UBOT CLIENT STARTED !!")
-
     await call_py.start()
     print("[INFO]: PY-TGCALLS CLIENT STARTED !!")
-
+    await user.join_chat("VeezSupportGroup")
+    await user.join_chat("levinachannel")
     await idle()
-
     print("[INFO]: STOPPING BOT & USERBOT")
     await bot.stop()
 
-
-if __name__ == "__main__":
-    asyncio.run(start_bot())
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
